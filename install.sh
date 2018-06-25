0693456d1f7b9fb8c3517c8dbcbea446f413cc17
kskdev <kosumikedev@gmail.com>
2018-06-26 00:07:25 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,39 @@
 #!/bin/sh
-" ----------------------------------------------------
-This file is an executable file for installing dein.vim
+# Standalone installer for Unixs
+# Referred :  https://github.com/shoma2da/neobundle_installer
 
-Install Command
-'sh ./exe.sh'
- ---------------------------------------------------- "
+# Set Absolute path
+INSTALL_ROOT=~/.vim/dein
 
-sh ./dein.sh ~/.vim/dein
+case $INSTALL_ROOT in
+  /*) PLUGIN_DIR=$INSTALL_ROOT;;
+  *) PLUGIN_DIR=$PWD/$INSTALL_ROOT;;
+esac
+
+INSTALL_DIR="${PLUGIN_DIR}/repos/github.com/Shougo/dein.vim"
+
+# check git command
+type git || {
+  echo 'Please install git or update your path to include the git executable!'
+  exit 1
+}
+
+# make plugin dir and fetch dein
+if ! [ -e "$INSTALL_DIR" ]; then
+  mkdir -p "$PLUGIN_DIR"
+  git clone https://github.com/Shougo/dein.vim "$INSTALL_DIR"
+fi
+
+
+echo "Complete setup dein!"
+
+
 cp ./vimrc ~/.vimrc
+
+# vim7.4のサポートをしなくなったらしいので，以下の2行の処理が必要
+cd ~/.vim/dein/repos/github.com/Shougo/dein.vim
+git checkout 1.0
+cd ~
+vim
 
 
