00367f7b204b47b7a35c5ea959a47f64bb0b5383
kskdev <kosumikedev@gmail.com>
2019-07-29 00:59:18 +0900

    support LSP with deoplete

--- install-neovim.sh
+++ install-neovim.sh
@@ -3,5 +3,6 @@
 
 mkdir -p ~/.config/nvim
 cp ./vimrc ~/.config/nvim/init.vim
-cp ./dein.toml ~/.config/nvim/dein.toml
+# cp ./dein.toml ~/.config/nvim/dein.toml
+cp ./deinLSP.toml ~/.config/nvim/dein.toml
 
--- install-win-neovim.sh
+++ install-win-neovim.sh
@@ -7,7 +7,7 @@
 TARGET_DIR="C:/Users/${USERNAME}/AppData/Local/nvim"
 TARGET_VIMRC="C:/Users/${USERNAME}/AppData/Local/nvim/init.vim"
 SOURCE_VIMRC="./vimrc"
-SOURCE_DEIN="./dein.toml"
+SOURCE_DEIN="./deinLSP.toml"
 TARGET_DEIN="C:/Users/${USERNAME}/AppData/Local/nvim/dein.toml"
 
 mkdir -p ${TARGET_DIR}
