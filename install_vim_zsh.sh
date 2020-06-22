85221a529c436fda1432bec3e3e470c927e6f725
kskdev <kosumikedev@gmail.com>
2020-06-23 00:07:02 +0900

    Add vim-lsp-settings. dont use powerfont in statusline. remove deoplete warnings.)

---
+++ install_vim_zsh.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# For Neovim
+
+case ${OSTYPE} in
+    darwin*)
+        # MacOS
+        SOURCE_VIMRC="./vimrc"
+        SOURCE_DEIN="./Plugins"
+        TARGET_DIR="${HOME}/.vim/"
+        TARGET_VIMRC="${HOME}/.vimrc"
+        TARGET_DEIN="${HOME}/.vim/dein/"
+
+        mkdir -p ${TARGET_DEIN}
+        cp ${SOURCE_VIMRC} ${TARGET_VIMRC}
+        cp -r ${SOURCE_DEIN} ${TARGET_DEIN}
+
+        # vim -e -c ":silent! call dein#install() | :q"
+        # vim -e -c ":Defx | :silent! UpdateRemotePlugins | :q"
+        ;;
+    linux*)
+        # Linux
+        SOURCE_VIMRC="./vimrc"
+        SOURCE_DEIN="./Plugins"
+        TARGET_DIR="${HOME}/.config/nvim"
+        TARGET_VIMRC="${HOME}/.config/nvim/init.vim"
+        TARGET_DEIN="${HOME}/.config/nvim/"
+
+        mkdir -p ${TARGET_DIR}
+        cp ${SOURCE_VIMRC} ${TARGET_VIMRC}
+        cp -r ${SOURCE_DEIN} ${TARGET_DEIN}
+
+        nvim -e -c ":silent! call dein#install() | :q!"
+        nvim -e -c ":Defx | :silent! UpdateRemotePlugins | :q"
+        ;;
+esac
+
